bf4625452d56e3edfa59f2cd5639cb4012b968f5
0zuth <breadjoo1014@naver.com>
2025-04-07 00:35:18 +0900

    chore: 환경 변수로 API proxy 설정 #6

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,34 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import viteCompression from "vite-plugin-compression";
 import svgr from "vite-plugin-svgr";
 import path from "path";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    svgr(),
-    viteCompression({
-      algorithm: "gzip",
-      ext: ".gz",
-    }),
-  ],
-  resolve: {
-    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [
+      react(),
+      svgr(),
+      viteCompression({
+        algorithm: "gzip",
+        ext: ".gz",
+      }),
+    ],
+    resolve: {
+      alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+    },
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_PUBLIC_API_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+          secure: false,
+        },
+      },
+    },
+  };
 });
