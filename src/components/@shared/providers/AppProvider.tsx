8989f6aca2c636b8f5473780025023f5f556756f
0zuth <breadjoo1014@naver.com>
2025-08-05 15:25:41 +0900

    feat: 페이지 로드 시 스크롤 위치 상단 고정 #15

--- src/components/@shared/providers/AppProvider.tsx
+++ src/components/@shared/providers/AppProvider.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter } from "react-router-dom";
 
 import AuthProvider from "@/components/@shared/providers/AuthProvider";
 import QueryProvider from "@/components/@shared/providers/QueryProvider";
+import ScrollToTop from "@/components/@shared/providers/ScrollToTop";
 import { Toaster } from "@/components/@shared/toast/toaster";
 
 interface AppProviderProps {
@@ -12,6 +13,7 @@ interface AppProviderProps {
 export default function AppProvider({ children }: AppProviderProps) {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <QueryProvider>
         <HelmetProvider>
           <AuthProvider>
