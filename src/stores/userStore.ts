59ddd2d40836341202b19cdb05906f8113bd6e7a
0zuth <breadjoo1014@naver.com>
2025-08-06 11:06:16 +0900

    test: 테스트 커밋

--- src/stores/userStore.ts
+++ src/stores/userStore.ts
@@ -3,5 +3,4 @@ import { atom } from "jotai";
 import { User } from "@/types/userDTO";
 
 export const userAtom = atom<User | null>(null);
-
 export const hasUserInfoAtom = atom((get) => !!get(userAtom));
